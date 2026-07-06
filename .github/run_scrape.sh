f57a66a3772c529066713c0feca957ecdc9f33c3
KryoMod <chuey316@gmail.com>
2026-07-06 16:54:07 -0400

    Delete .github/run_scrape.sh

--- .github/run_scrape.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 scripts/import_exfat.py --out dlps-exfat.json
-python3 scripts/scrape_superpsx.py --out superpsx.json
-
-python3 scripts/merge_catalogs.py final_catalog.json dlps-exfat.json superpsx.json
