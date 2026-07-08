09f114443114b0f09bdca27698562d58b3d76ca6
KryoMod <chuey316@gmail.com>
2026-07-08 11:33:54 -0400

    Update run_scrape.sh

--- run_scrape.sh
+++ run_scrape.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 scripts/import_exfat.py --out dlps-exfat.json
-python3 scripts/scrape_superpsx.py --out superpsx.json
+python3 scripts/scrape_superpsx.py --out superpsx.json --http-backend flaresolverr
 
 python3 scripts/merge_catalogs.py
